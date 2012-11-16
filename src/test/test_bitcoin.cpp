8e1d760467080f42912bc30fc38fe2913e0fff35
Philip Kaufmann <phil.kaufmann@t-online.de>
2012-11-16 21:46:16 +0100

    ConnectBlock(): fix error() format to be unsigned
    - I introduced the wrong format macro with my former patch (#2018), this
      needs to be signed not unsigned (thanks Luke-Jr)

---
+++ src/test/test_bitcoin.cpp
@@ -0,0 +1,53 @@
+#define BOOST_TEST_MODULE Bitcoin Test Suite
+#include <boost/test/unit_test.hpp>
+
+#include "db.h"
+#include "txdb.h"
+#include "main.h"
+#include "wallet.h"
+
+CWallet* pwalletMain;
+CClientUIInterface uiInterface;
+
+extern bool fPrintToConsole;
+extern void noui_connect();
+
+struct TestingSetup {
+    CCoinsViewDB *pcoinsdbview;
+
+    TestingSetup() {
+        fPrintToDebugger = true; // don't want to write to debug.log file
+        noui_connect();
+        bitdb.MakeMock();
+        pblocktree = new CBlockTreeDB(1 << 20, true);
+        pcoinsdbview = new CCoinsViewDB(1 << 23, true);
+        pcoinsTip = new CCoinsViewCache(*pcoinsdbview);
+        LoadBlockIndex();
+        bool fFirstRun;
+        pwalletMain = new CWallet("wallet.dat");
+        pwalletMain->LoadWallet(fFirstRun);
+        RegisterWallet(pwalletMain);
+    }
+    ~TestingSetup()
+    {
+        delete pwalletMain;
+        pwalletMain = NULL;
+        delete pcoinsTip;
+        delete pcoinsdbview;
+        delete pblocktree;
+        bitdb.Flush(true);
+    }
+};
+
+BOOST_GLOBAL_FIXTURE(TestingSetup);
+
+void Shutdown(void* parg)
+{
+  exit(0);
+}
+
+void StartShutdown()
+{
+  exit(0);
+}
+
