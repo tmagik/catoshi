a2665edea436f560c53ad29e8b077c25dc399e30
Troy Benjegerdes <hozer@hozed.org>
2015-02-11 00:38:20 -0600

    Catch some missed branding missteps

--- src/qt/askpassphrasedialog.cpp
+++ src/qt/askpassphrasedialog.cpp
@@ -104,7 +104,7 @@ void AskPassphraseDialog::accept()
             break;
         }
         QMessageBox::StandardButton retval = QMessageBox::question(this, tr("Confirm wallet encryption"),
-                 tr("WARNING: If you encrypt your wallet and lose your passphrase, you will <b>LOSE ALL OF YOUR PEERCOINS</b>!\nAre you sure you wish to encrypt your wallet?"),
+                 tr("WARNING: If you encrypt your wallet and lose your passphrase, you will <b>LOSE ALL OF YOUR COINS</b>!\nAre you sure you wish to encrypt your wallet?"),
                  QMessageBox::Yes|QMessageBox::Cancel,
                  QMessageBox::Cancel);
         if(retval == QMessageBox::Yes)
@@ -114,7 +114,7 @@ void AskPassphraseDialog::accept()
                 if(model->setWalletEncrypted(true, newpass1))
                 {
                     QMessageBox::warning(this, tr("Wallet encrypted"),
-                                         tr("Blunity will close now to finish the encryption process. Remember that encrypting your wallet cannot fully protect your Bluecoins from being stolen by malware infecting your computer."));
+                                         tr("Blunity will close now to finish the encryption process. Remember that encrypting your wallet cannot fully protect your BlueCoins from being stolen by malware infecting your computer."));
                     QApplication::quit();
                 }
                 else
