a02163c7246893d7e9ae72b90b08f0017d1745ba
Wladimir J. van der Laan <laanwj@gmail.com>
2015-09-03 21:43:12 +0200

    Merge pull request #6630
    86270c8 Replace boost::reverse_lock with our own. (Casey Rodarmor)

--- qa/pull-tester/rpc-tests.sh
+++ qa/pull-tester/rpc-tests.sh
@@ -5,13 +5,8 @@ CURDIR=$(cd $(dirname "$0"); pwd)
 # Get BUILDDIR and REAL_BITCOIND
 . "${CURDIR}/tests-config.sh"
 
-export BITCOINCLI=${BUILDDIR}/qa/pull-tester/run-bitcoin-cli
 export BITCOIND=${REAL_BITCOIND}
-
-if [ "x${EXEEXT}" = "x.exe" ]; then
-  echo "Win tests currently disabled"
-  exit 0
-fi
+export BITCOINCLI=${REAL_BITCOINCLI}
 
 #Run the tests
 
