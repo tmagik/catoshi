84d8791b08042ed1a28d07483acb97aabe620d26
Troy Benjegerdes <hozer@hozed.org>
2015-12-19 01:34:23 -0600

    placeholder for more p2pool updates

--- contrib/p2pool/p2pool/bitcoin/networks.py
+++ contrib/p2pool/p2pool/bitcoin/networks.py
@@ -270,6 +270,27 @@ nets = dict(
         DUST_THRESHOLD=0.01e6,
     ),
 #TODO insert_grantcoin_here
+    grantcoin=math.Object(
+        P2P_PREFIX='e4e8e9e5'.decode('hex'),
+        P2P_PORT=7777,
+        ADDRESS_VERSION=8,
+        RPC_PORT=8344,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            0 == (yield bitcoind.rpc_getblock('00000a060336cbb72fe969666d337b87198b1add2abaa59cca226820b32933a4'))['height'] and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda bitcoind, target: get_subsidy(bitcoind, target),
+        BLOCK_PERIOD=600, # s
+        SYMBOL='NVC',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'NovaCoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/NovaCoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.novacoin'), 'novacoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://explorer.novaco.in/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.novaco.in/address/',
+        TX_EXPLORER_URL_PREFIX='http://explorer.novaco.in/tx/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=0.01e6,
+        MULTIPLIER=1e6,  #1e6 for PPC-derivatives, 1e8 for bitcoin
+    ),
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
