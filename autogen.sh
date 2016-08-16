9bb97d21b0d531c35ecab146f2bcc189f1eabc1f
Wladimir J. van der Laan <laanwj@gmail.com>
2016-08-16 11:34:12 +0200

    Merge commit 'b2135359b3ad37cf2ac09b008079ddb237eff2c9'

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 set -e
-autoreconf -if --warnings=all
+srcdir="$(dirname $0)"
+cd "$srcdir"
+if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
+  LIBTOOLIZE="${GLIBTOOLIZE}"
+  export LIBTOOLIZE
+fi
+which autoreconf >/dev/null || \
+  (echo "configuration failed, please install autoconf first" && exit 1)
+autoreconf --install --force --warnings=all
