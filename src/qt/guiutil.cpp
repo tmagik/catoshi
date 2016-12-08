17189ce19b2db7fe87480a22db6037f48e0510fc
Wladimir J. van der Laan <laanwj@gmail.com>
2016-12-08 07:43:50 +0100

    Merge #9276: Some minor testing cleanups
    30b620c remove obsolete run-bitcoind-for-test.sh (Alex Morcos)
    2a99522 remove relaypriority from rpc tests (Alex Morcos)
    e2184cc Reorder RPC tests for running time (Alex Morcos)

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -55,6 +55,7 @@
 #include <QSettings>
 #include <QTextDocument> // for Qt::mightBeRichText
 #include <QThread>
+#include <QMouseEvent>
 
 #if QT_VERSION < 0x050000
 #include <QUrl>
@@ -986,4 +987,15 @@ QString formateNiceTimeOffset(qint64 secs)
     }
     return timeBehindText;
 }
+
+void ClickableLabel::mouseReleaseEvent(QMouseEvent *event)
+{
+    Q_EMIT clicked(event->pos());
+}
+    
+void ClickableProgressBar::mouseReleaseEvent(QMouseEvent *event)
+{
+    Q_EMIT clicked(event->pos());
+}
+
 } // namespace GUIUtil
